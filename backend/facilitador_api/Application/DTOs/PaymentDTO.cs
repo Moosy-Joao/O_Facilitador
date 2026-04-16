d174b8e03e4c1256e2ac8d68c219c914a731159a
Joao Pedro <jpp15marques@gmail.com>
2026-04-15 22:06:41 -0300

    :sparkles: feat: Registro de Pagamento(Em andamento)

---
+++ backend/facilitador_api/Application/DTOs/PaymentDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace facilitador_api.Application.DTOs
+{
+    public class PaymentDTO
+    {
+        public decimal Valor { get; set; }
+        public bool Tipo { get; set; }
+    }
+}
---
+++ backend/facilitador_api/Infrastructure/Repositories/PaymentRepository.cs
@@ -0,0 +1,28 @@
+﻿using facilitador_api.Domain.Entities;
+using facilitador_api.Domain.Interfaces;
+using facilitador_api.Infrastructure.DB;
+using System.Linq;
+
+namespace facilitador_api.Infrastructure.Repositories;
+
+public class PaymentRepository : IPaymentRepository
+{
+    private readonly ConnectionContext _context;
+
+    public PaymentRepository(ConnectionContext context)
+    {
+        _context = context;
+    }
+
+    public void Adicionar(Cliente cliente)
+    {
+        _context.Clientes.Add(cliente);
+        _context.SaveChanges();
+    }
+
+    public Payment? ObterPorDocumento(string documento)
+    {
+        return _context.Clientes
+            .FirstOrDefault(c => c.Documento == documento);
+    }
+}
